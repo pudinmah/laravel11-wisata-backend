eeaf3de7ce535192c95ce5d09b0a80392c8dddf5
pudinmah <pudinmah69@gmail.com>
2024-06-26 23:05:17 +0700

    Laravel Fortify

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('pages.dashboard');
+// });
+
 Route::get('/', function () {
-    return view('pages.dashboard');
+    return view('pages.auth.login');
 });
